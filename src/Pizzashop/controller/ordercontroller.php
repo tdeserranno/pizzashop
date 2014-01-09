969546bab025d73da80826759d9843039d485df3
unknown <cyber02@sat02.CERES.INTRA>
2014-01-09 16:27:01 +0100

    EOD 9-1-2014

---
+++ src/Pizzashop/controller/ordercontroller.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Pizzashop\Controller;
+use Library\Controller;
+
+/**
+ * Description of ordercontroller
+ *
+ * @author cyber02
+ */
+class OrderController extends Controller
+{
+    //put your code here
+}
